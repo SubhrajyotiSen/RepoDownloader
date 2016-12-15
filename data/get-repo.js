a551987ef88ac4c42ca91bcc9412bd876f104bb3
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2016-12-15 13:35:15 +0530

    feat: improve error handling and add clear button

--- data/get-repo.js
+++ data/get-repo.js
@@ -1,26 +1,33 @@
 function clickHandler(){
-
-
-  	var urlField = document.getElementById('url');
-  	var url = null;
-  	if(urlField!=null){
-  		 url = urlField.value;
-  	}
-  	else{
-  		alert("Empty url field!");
-  		console.log("Empty url field!");
-
-  	}
-    console.log("url :"+url);
-    GitZip.zipRepo(url);
+	var urlField = document.getElementById('url');
+	var url = null;
+	if(urlField!=null){
+		 url = urlField.value;
+	}
+	console.log("url :"+url);
+  var repoExp = new RegExp("https://github.com/[^/]+/[^/]+/(tree|blob)/.+");
+  if (!repoExp.test(url)) {
+    console.log("incorrect",url);
+    alert("Not a GitHub sub-repository");
   }
-  	
+  else
+    GitZip.zipRepo(url);   
+}
 
+function clearText(){
+  var urlField = document.getElementById('url');
+  urlField.value="";
+}
+  	
 document.addEventListener('click',function () {
 	var button = document.getElementById('submit');
-	if(button){
+  var clear = document.getElementById('clear');
+	if(button && clear){
   		button.addEventListener('click', clickHandler);
+      clear.addEventListener('click',clearText);
   	}else{
   		console.log("button is null");
   	}
   })
+
+
